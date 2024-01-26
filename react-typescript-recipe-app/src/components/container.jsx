eabd6b4d37a2ff5aa5fe58643ac3ce15e8479d1d
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2024-01-25 21:50:24 -0500

    Setting up initial structure and css

--- react-typescript-recipe-app/src/components/container.jsx
+++ react-typescript-recipe-app/src/components/container.jsx
@@ -1,12 +1,26 @@
 import { Footer } from "./footer";
 import { Header } from "./header";
+import { recipeData } from "../data";
+import '../App.css';
 
 export const Container = () => {
     return (
         <>
             <Header/>
             <main className ="">
-
+                <div className="grid-container">
+                    {recipeData.map(recipe => {
+                        return(
+                            <div className="recipe-card" key={recipe.id}>
+                                <p>{recipe.name}</p>
+                                <p>{recipe.description}</p>
+                                {recipe.ingredients.map(ingredient => {
+                                    return(<p>{ingredient}</p>);
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
             </main>
             <Footer/>
         </>
